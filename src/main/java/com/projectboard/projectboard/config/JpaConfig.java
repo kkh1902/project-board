b936a187cd2e4a70d5aeb0d6f29e9d82230f3c76
gihun Kim <hunjyhunji@gmail.com>
2025-04-05 06:52:11 +0900

    #10  - Sprind Data JPA 속성과 설정
    * db 접근과 개발 편의를 위한 각종 부트 프로퍼티 적용
    * TODO : 패스워드를 프로퍼티에 노출 시켰다. 처리 필요

---
+++ src/main/java/com/projectboard/projectboard/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package com.projectboard.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of("uno"); //
+    }
+}
